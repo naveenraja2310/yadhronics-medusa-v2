2dddc03037a02a44393193d4dfbbfe0e395681b3
naveenraja2310 <109686235+naveenraja2310@users.noreply.github.com>
2025-09-06 12:12:51 +0530

    Added .medusa folder

---
+++ .medusa/server/src/api/hello-world/route.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GET = void 0;
+const GET = (req, res) => {
+    res.json({
+        message: "Hello world!!",
+    });
+};
+exports.GET = GET;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm91dGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9zcmMvYXBpL2hlbGxvLXdvcmxkL3JvdXRlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUtPLE1BQU0sR0FBRyxHQUFHLENBQ2YsR0FBa0IsRUFDbEIsR0FBbUIsRUFDckIsRUFBRTtJQUNBLEdBQUcsQ0FBQyxJQUFJLENBQUM7UUFDTCxPQUFPLEVBQUUsZUFBZTtLQUMzQixDQUFDLENBQUE7QUFDTixDQUFDLENBQUE7QUFQWSxRQUFBLEdBQUcsT0FPZiJ9
