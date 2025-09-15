0da7ede7b82435cd415c46224fa01e977c17bc98
kiran kumar <kirankumar.dm@apexon.com>
2025-09-15 14:02:10 +0530

    updated confirm email

---
+++ .medusa/server/public/admin/assets/chunk-RQYKGJZ4-CbN_jZg6.js
@@ -0,0 +1,1 @@
+import{aC as i}from"./index-S3JTAn61.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- .medusa/server/public/admin/assets/chunk-RQYKGJZ4-DFW6qTdF.js
+++
@@ -1,1 +0,0 @@
-import{aC as i}from"./index-DFch3EGl.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
